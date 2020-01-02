7d905b7d02f9a904366d1e1e3ef9ca715cf6c2a7
UcroX <ucroxlive@gmail.com>
2020-01-02 12:12:18 -0500

    Crea la ruta DELETE /products/:id

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   post '/products', to: 'products#create'
   get '/products/:id', to: 'products#show', as: 'product'
   get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
-  patch 'products/:id', to: 'products#update'
+  patch '/products/:id', to: 'products#update'
+  delete '/products/:id', to: 'products#destroy'
 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
