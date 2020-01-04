b5e440bd2def00b6544d006815ed31251d2de1c8
UcroX <ucroxlive@gmail.com>
2020-01-04 12:16:37 -0500

    Anota los modelos con los campos de la BD

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  url         :string
+#  description :text
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Product < ActiveRecord::Base
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  password_digest :string
+#  name            :string
+#  twitter_handle  :string
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class User < ApplicationRecord
   has_secure_password validations: false
 end
